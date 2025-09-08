9ea72d847c1758f2e993951c9b347ac7cca18c38
WorkGitesh <work.giteshpradhan@gmail.com>
2025-09-09 00:28:20 +0530

    work uri setup in tain.py file

--- mlops-project-2/train_mlflow.py
+++ mlops-project-2/train_mlflow.py
@@ -7,6 +7,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
+mlflow.set_tracking_uri("file:./mlruns")
 # Set the experiment name in MLflow
 mlflow.set_experiment("Iris Flower Classification")
 
